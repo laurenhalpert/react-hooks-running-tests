e445231cba2e2b05a3097b29e6c937015c5c8d34
Lauren <halpertln@gmail.com>
2023-03-17 10:19:19 -0600

    now complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,12 +4,13 @@ import React from "react";
 
 //import Article from "../components/Article";
 function Article (props) {
-  return <p>please pass this test</p>
+  return <div>please pass this test</div>
 }
 export default Article
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
 });
